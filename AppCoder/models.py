76afab9a18229f63dc0aa616a68be5d7f9fed2aa
Joaco95 <88909293+Joaco95@users.noreply.github.com>
2022-01-22 22:13:11 -0300

    prueba joaquin

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -8,13 +8,17 @@ from django.db.models.fields import (
     BooleanField, CharField, DateField, EmailField, IntegerField,
     GenericIPAddressField, URLField, DecimalField, TimeField
 )
-
+###Lo hizo Jesus
 class reseñas(models.Model):
     Mensaje = models.CharField(max_length=300)
     Email = models.EmailField(max_length=300)
     Fecha = models.DateTimeField("date logged") 
     
-    
+###Lo hizo Joaquin
+class reseñas(models.Model):
+    nombre = models.CharField(max_length=300)
+    Email = models.EmailField(max_length=300)
+    Fecha = models.DateTimeField() 
 #def __str__(self):
 #        """Returns a string representation of a message."""
  #       date = timezone.localtime(self.log_date)
